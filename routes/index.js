44809387128a4321c36aa86d7546d24632e6e96b
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2024-04-19 03:53:19 +0300

    add template messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Armando",
+    added: new Date(),
+  },
+  {
+    text: "Hello Express",
+    user: "Dante",
+    added: new Date(),
+  },
+];
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Message Board", messages: messages });
 });
 
 module.exports = router;
